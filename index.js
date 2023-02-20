d4026e0e0c0b9f04875b2b0cca2ca41a88dc4d1b
coder-emon <developeremon.me@gmail.com>
2023-02-20 22:31:57 +0600

    jot token implement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -8,6 +9,26 @@ const port = process.env.PORT || 5000;
 // middle ware 
 app.use(cors())
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+
+
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+
+            return res.status(403).send({ message: "Access Forbidden", err: err?.message })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ixsnvhr.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,6 +46,7 @@ async function dbConnect() {
         })
     }
 }
+dbConnect().catch(console.dir())
 const servicesCollection = client.db("geniusCar").collection("services")
 const ordersCollection = client.db("geniusCar").collection("orders");
 app.post("/orders", async (req, res) => {
@@ -46,25 +68,32 @@ app.post("/orders", async (req, res) => {
         })
     }
 })
-app.get("/orders", async (req, res) => {
+app.get("/orders", verifyJWT, async (req, res) => {
     try {
+        const decoded = req.decoded;
+
+        if (decoded?.email !== req.query?.email) {
+            res.status(401).send("unauthorized access onnno manser secret use korco kn")
+        }
         let query = {}
         if (req.query.email) {
             query = { email: req.query.email }
         }
         const cursor = ordersCollection.find(query)
         const orders = await cursor.toArray()
+        console.log(orders);
         res.send({
             success: true,
             message: "Successfully got orders data",
-            data: orders
+            data: orders,
+
         })
     }
     catch (err) {
         console.error(err)
         res.send({
             success: false,
-            message: err.message
+            message: `${err.message}`
         })
     }
 })
@@ -145,6 +174,24 @@ app.get("/services/:id", async (req, res) => {
             success: true,
             message: "Successfully got service dat",
             data: service
+        })
+    }
+    catch (err) {
+        console.error(err)
+        res.send({
+            success: false,
+            message: err.message
+        })
+    }
+})
+app.post("/jwt", (req, res) => {
+    try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+        res.send({
+            success: true,
+            message: "succesfully get access token",
+            data: token,
         })
     }
     catch (err) {
